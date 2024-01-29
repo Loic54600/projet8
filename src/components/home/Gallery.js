179366c4cb8398d6700390ab14bcf613afafd9eb
loic guervin <odboy_54600@hotmail.fr>
2024-01-29 09:47:51 +0100

    verification gallery

--- src/components/home/Gallery.js
+++ src/components/home/Gallery.js
@@ -1,58 +1,20 @@
 import React from 'react';
-{/*import logements from "../../src/datas/logement.json";*/}
+import logements from "../../../src/datas/logement.json";
+
+
+
 
 
 const Gallery = () => {
     return (
         <section className="gallery">
-            <div className="gallery_cards">
-                <section>
-                    <div className="grid">
-                        {/*Card*/ }
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                        <div className="grid_item">
-                            <div class="container">
-                                <h4><b>Titre de la</b></h4>
-                                <h4><p>location</p></h4>
-                            </div>
-                        </div>
-                       
-
-
-                
-
-                    </div>
-                </section>
-            </div>
+            {logements.map((logement) => {
+        return (
+          <article key={logement.id}>
+              <div image={logement.cover} title={logement.title} />
+          </article>
+        );
+      })}
         </section>
     );
 };
