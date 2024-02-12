31690b38ef802fd711f532f64bda2ea0f37f318a
loic guervin <odboy_54600@hotmail.fr>
2024-02-12 11:30:31 +0100

    carrousel 2.0

--- src/components/location/Carrousel.js
+++ src/components/location/Carrousel.js
@@ -10,28 +10,27 @@ function Carrousel({ pictures }) {
 	const totalPictures = pictures.length - 1
 
 	const nextSlide = () => {
-        setIndex(index + 1)
-        if(index === pictures.length - 1)
-            setIndex(0)
-    }
+		setIndex(index + 1)
+		if (index === pictures.length - 1)
+			setIndex(0)
+	}
 
-    const prevSlide = () => {
-        setIndex(index - 1)
-        if(index === 0)
-            setIndex(pictures.length - 1)
-    }
+	const prevSlide = () => {
+		setIndex(index - 1)
+		if (index === 0)
+			setIndex(pictures.length - 1)
+	}
 
 	if (index < 0) setIndex(totalPictures)
 	if (index > totalPictures) setIndex(0)
 
 	return (
 		<div className='carrousel'>
-
 			<div className='div-image'>
 				<img src={pictures[index]} className="classImage" key={"car-" + index} alt={"photo " + index} />
 			</div>
 
-			{totalPictures > 0 && (
+			{totalPictures.length > 0 && (
 				<div>
 					<button onClick={(nextSlide) => setIndex(index - 1)}>{index}
 						<img src={left} className='classleft' alt={'flèche gauche' + index} />
@@ -45,7 +44,7 @@ function Carrousel({ pictures }) {
 
 				<div className='div-compteur'>
 					<p className='compteurImages'>
-						{index + 1}/{totalPictures + 1}
+						{index + 1}/{totalPictures.length + 1}
 					</p>
 				</div>
 			)}
